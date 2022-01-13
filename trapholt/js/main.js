067900dc239bce44d5ee03abf2d7bee03ae06547
xNans <95973544+xNans@users.noreply.github.com>
2022-01-13 14:40:09 +0100

    fucks

--- trapholt/js/main.js
+++ trapholt/js/main.js
@@ -1,52 +1,8 @@
-let darkMode = () => {
-  let element = document.getElementsByClassName("body")[0] // [0] cuz class are going through an array
-  // let element = document.body 
-  element.classList.toggle("dark");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* var myFunction = () => {
-  let showcaseBlock = document.getElementsByClassName("showcase")[0] // add [0], since: getElementsByClassName() method returns a collection of all elements in the document with the specified class name, as an HTMLCollection object.
-  let myDivObjBgColor = window.getComputedStyle(showcaseBlock).backgroundColor;
-  console.log("bg = ", myDivObjBgColor)
-  
-  //document.getElementsByClassName("showcase")[0].style.backgroundColor = "red"
-  //showcaseBlock.style.backgroundColor = "#9ec1f9" 
-    if (myDivObjBgColor == "rgb(0, 0, 0)") {
-     document.getElementsByClassName("showcase")[0].style.backgroundColor = "rgb(158, 193, 249)"
-
-  }
-  else if (myDivObjBgColor == "rgb(158, 193, 249)") {
-    document.getElementsByClassName("showcase")[0].style.backgroundColor = "rgb(0, 0, 0)"
-    
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
-/*
-if (document.body.style.background=="white"){
-  document.body.style.background="purple";
-}else if(document.body.style.background=="purple"){
-  document.body.style.background="white";
-}
-*/
